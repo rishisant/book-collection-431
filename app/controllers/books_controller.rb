baed4c7b62e4aa4ff2c51a138ce5bcd09f833d4b
Rishi Santhanam <rishisant@tamu.edu>
2024-09-05 21:56:40 -0500

    [FIX] Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,58 +8,46 @@ class BooksController < ApplicationController
     end
 
     def create
-        title = params[:book][:title]
-        author = params[:book][:author]
-        price = params[:book][:price]
-        # date
-        published = params[:book][:published]
-        begin
-            @book = Book.new(
-                title: title,
-                author: author,
-                price: price,
-                published: published
-            )
-            if @book.save
-                redirect_to books_path, notice: "Book created successfully."
-            else
-                render :new
-            end
-        rescue
-            render :new
+        @book = Book.new(book_params)  # Use strong parameters
+        if @book.save
+          redirect_to books_path, notice: "Book created successfully."
+        else
+          render :new
+        end
+      end
+    
+        
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to books_path, notice: "Book updated successfully."
+        else
+            render :edit
         end
     end
-def edit
-    @book = Book.find(params[:id])
-end
 
-def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-        redirect_to books_path, notice: "Book updated successfully."
-    else
-        render :edit
+    def show
+        @book = Book.find(params[:id])
     end
-end
 
-def show
-    @book = Book.find(params[:id])
-end
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path, notice: "Book deleted successfully."
+    end
 
-def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path, notice: "Book deleted successfully."
-end
+    private
 
-private
-
-def book_params
-    params.require(:book).permit(
-        :title,
-        :author,
-        :price,
-        :published
-    )
-end
-end
+    def book_params
+        params.require(:book).permit(
+            :title,
+            :author,
+            :price,
+            :published
+        )
+    end
+    end
