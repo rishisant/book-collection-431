c8a58779c40d03077105d210496ac305931acfb7
Rishi Santhanam <rishisant@tamu.edu>
2024-09-05 21:52:27 -0500

    [ADD] Stylesheets

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,65 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        title = params[:book][:title]
+        author = params[:book][:author]
+        price = params[:book][:price]
+        # date
+        published = params[:book][:published]
+        begin
+            @book = Book.new(
+                title: title,
+                author: author,
+                price: price,
+                published: published
+            )
+            if @book.save
+                redirect_to books_path, notice: "Book created successfully."
+            else
+                render :new
+            end
+        rescue
+            render :new
+        end
+    end
+def edit
+    @book = Book.find(params[:id])
+end
+
+def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+        redirect_to books_path, notice: "Book updated successfully."
+    else
+        render :edit
+    end
+end
+
+def show
+    @book = Book.find(params[:id])
+end
+
+def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "Book deleted successfully."
+end
+
+private
+
+def book_params
+    params.require(:book).permit(
+        :title,
+        :author,
+        :price,
+        :published
+    )
+end
 end
